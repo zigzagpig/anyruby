612d363255382afec48da8b33fa6a1993007babd
zigzagpig <695312886@qq.com>
2015-04-20 09:14:42 +0800

    修改了路由，去掉了不需要的路径

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-
-  get 'password_resets/new'
-
-  get 'password_resets/edit'
-
   root 							'static_pages#home'
   get  		'help'			=>  'static_pages#help'
   get  		'about'			=>	'static_pages#about'
